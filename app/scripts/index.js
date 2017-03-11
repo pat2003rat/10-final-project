4af9b81ca520b3aed09703e450398ea8ea758568
Patrick Ratigan <patrickratigan@Patricks-MacBook-Pro.local>
2017-03-11 17:47:13 -0500

    setting up scaffolding and auth practice

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+
+var $ = require('jquery');
+
+var apiUrl = 'http://api.wunderground.com/api/1a11b11566a747ab/conditions/q/SC/Inman.json'
+
+$.get({
+  url: apiUrl,
+  dataType: 'jsonp'
+}).done(function(data){
+  console.log(data.current_observation.icon_url);
+});
