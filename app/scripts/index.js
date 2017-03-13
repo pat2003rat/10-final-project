32c49ca74695bdf8137ad649f2832af96bfc7436
Patrick Ratigan <patrickratigan@Patricks-MacBook-Pro.local>
2017-03-13 11:23:54 -0400

    adding user token

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -9,3 +9,76 @@ $.get({
 }).done(function(data){
   console.log(data.current_observation.icon_url);
 });
+
+function setupAjax(loggedInUser){
+  $.ajaxSetup({
+      beforeSend: function(xhr){
+        xhr.setRequestHeader("X-Parse-Application-Id", "tiygvl");
+        xhr.setRequestHeader("X-Parse-REST-API-Key", "slumber");
+        if(loggedInUser){
+          xhr.setRequestHeader("X-Parse-Session-Token", loggedInUser.sessionToken);
+        }
+      }
+  });
+}
+
+setupAjax();
+
+var apiUrl = 'https://tiny-parse-server.herokuapp.com';
+
+// $.get(apiUrl + '/Puppy').then(function(data){
+//   console.log(data);
+// });
+//
+// $.post(apiUrl + '/Puppy', {'name': 'Kepler'}).then(function(data){
+//   console.log(data);
+// });
+
+$('#signup').on('submit', function(e){
+  e.preventDefault();
+
+  var username = $('#signup-email').val();
+  var password = $('#signup-password').val();
+  var user = {
+    username: username,
+    password: password
+  };
+
+  $.post(apiUrl + '/users', user).then(function(data){
+    console.log(data);
+  });
+
+});
+
+$('#login').on('submit', function(e){
+  e.preventDefault();
+
+  var username = $('#email-login').val();
+  var password = $('#password-login').val();
+  console.log(username);
+  console.log(password);
+
+  var url = apiUrl + '/login?username=' +
+            encodeURIComponent(username) + '&' +
+            'password=' + encodeURIComponent(password);
+
+  console.log(url);
+
+  $.get(url).then(function(data){
+    console.log(data);
+    var userData = JSON.stringify(data);
+    localStorage.setItem('user', userData);
+  });
+});
+
+var loggedInUser = localStorage.getItem('user');
+
+if(loggedInUser){
+  loggedInUser = JSON.parse(loggedInUser);
+  loggedInUser.sessionToken;
+  setupAjax(loggedInUser);
+
+  $.get(apiUrl + '/users/me').then(function(data){
+    console.log(data);
+  })
+}
---
+++ app/scripts/router.js
@@ -0,0 +1,6 @@
+// Third Party Libraries
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Backbone = require('backbone');
+
+//Controllers
