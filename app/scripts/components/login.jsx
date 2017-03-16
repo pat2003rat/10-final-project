2bdb8a326360916b64a64605c5e3f22b77ed1f50
git-directory-deploy <patrickratigan@Patricks-MacBook-Pro.local>
2017-03-16 12:02:49 -0400

    working on userAccount screen

--- app/scripts/components/login.jsx
+++ app/scripts/components/login.jsx
@@ -5,6 +5,34 @@ var Backbone = require('backbone');
 var User = require('../models/user');
 
 class Login extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.handleUsername= this.handleUsername.bind(this);
+    this.handlePassword= this.handlePassword.bind(this);
+    this.handleLogin= this.handleLogin.bind(this);
+
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleUsername(e) {
+    this.setState({username: e.target.value});      //this is the value the user just typed in
+  }
+
+  handlePassword(e) {
+    this.setState({password: e.target.value});
+  }
+
+  handleLogin(e) {
+    e.preventDefault();
+    User.login(this.state, function(){
+      Backbone.history.navigate('userAccount/', { trigger: true} );
+    });
+  }
+
   render() {
     return (
       <div>
@@ -18,20 +46,22 @@ class Login extends React.Component {
 
         <div className="container">
           <div className="row">
+            <div className="col-md-12 text-center">
             <div className="col-md-6">
               <h1>Please Login</h1>
-              <form id="login">
+              <form id="login" onSubmit={this.handleLogin}>
                 <div className="form-group">
                   <label htmlFor="email-login">Email address</label>
-                  <input className="form-control" name="email" id="email-login" type="email" placeholder="email"/>
+                  <input onChange={this.handleUsername} value={this.state.username} className="form-control" name="email" id="email-login" type="email" placeholder="email"/>
                 </div>
                 <div className="form-group">
                   <label htmlFor="password-login">Password</label>
-                  <input className="form-control" name="password" id="password-login" type="password" placeholder="Password Please"/>
+                  <input onChange={this.handlePassword} value={this.state.password} className="form-control" name="password" id="password-login" type="password" placeholder="Password Please"/>
                 </div>
-                <input className="btn btn-primary" type="submit" value="Beam Me Up!"/>
+                <input className="btn btn-primary" type="submit" value="Log Me In!"/>
               </form>
             </div>
+          </div>
           </div>
           <SignUp></SignUp>
         </div>
@@ -42,50 +72,55 @@ class Login extends React.Component {
 
 class SignUp extends React.Component {
   constructor(props){
-    super(props)
-    this.handleUsername= this.handleUsername.bind(this)
-    this.handlePassword= this.handlePassword.bind(this)
-    this.handleSubmit= this.handleSubmit.bind(this)
+    super(props);
+
+    this.handleUsername= this.handleUsername.bind(this);
+    this.handlePassword= this.handlePassword.bind(this);
+    this.handleSignup= this.handleSignup.bind(this);
+
     this.state = {
       username: '',
       password: ''
     };
   }
 
-  handleUsername(e){
-  this.setState({username: e.target.value});      //this is the value the user just typed in
-}
+  handleUsername(e) {
+    this.setState({username: e.target.value});      //this is the value the user just typed in
+  }
 
-  handlePassword(e){
-  this.setState({password: e.target.value});
-}
+  handlePassword(e) {
+    this.setState({password: e.target.value});
+  }
 
-handleSubmit(e){
-  e.preventDefault();
-  var user = new User(this.state);
-  user.save().then(function(data){
-    localStorage.setItem('user', JSON.stringify(data));
-    Backbone.history.navigate('userAccount/', { trigger: true })
-  });
-}
+  handleSignup(e){
+    e.preventDefault();
+
+    var user = new User(this.state);
+
+    user.save().then(function(data){
+
+      localStorage.setItem('user', JSON.stringify(data));
+      Backbone.history.navigate('userAccount/', { trigger: true })
+    });
+  }
 
   render() {
     return (
       <div>
         <div className="col-md-6">
           <h1>No Account? Sign Up!</h1 >
-          <form onSubmit={this.handleSubmit} id="signup">
+          <form onSubmit={this.handleSignup} id="signup">
             <div className="form-group">
               <label htmlFor="email">Email</label>
-              <input onChange={this.handleUsername} id="signup-email" className="form-control" type="text" name="email" placeholder="Email Address"/>
+              <input onChange={this.handleUsername} id="signup-email" className="form-control" type="email" name="email" placeholder="Email Address" required />
             </div>
             <div className="form-group">
               <label htmlFor="email">Password</label>
-              <input onChange={this.handlePassword} id="signup-password" className="form-control" type="password" name="password" placeholder="Don't Share This!"/>
+              <input onChange={this.handlePassword} id="signup-password" className="form-control" type="password" name="password" placeholder="Don't Share This!" required/>
             </div>
             <input className="btn btn-primary" type="submit" value="Sign Up!"/>
           </form>
-        </div>
+         </div>
       </div>
 
     )
@@ -93,6 +128,5 @@ handleSubmit(e){
 }
 
   module.exports = {
-    Login,
-    SignUp
+    Login
   };
--- app/scripts/components/useraccount.jsx
+++ app/scripts/components/useraccount.jsx
@@ -1,3 +1,4 @@
+
 var $ = require('jquery');
 var React = require('react');
 var Backbone = require('backbone');
@@ -6,9 +7,32 @@ class UserAccount extends React.Component {
     render(){
       return (
         <div className="container">
-          Not the Splash Screen
+          <div className="title text-center">
+            <h1>Training Dean</h1>
+          </div>
+
+          <div id="pagewrap">
+            <header>
+      		    <h2>Welcome to your Training Sessions</h2>
+      	    </header>
+      	    <section id="content">
+              <div className="col-md-5">
+      		      <h2>Schedule</h2>
+              </div>
+      	    </section>
+            <section id="middle">
+              <div className="col-md-2 text-center">
+                <h2>Weather</h2>
+              </div>
+            </section>
+          <div className="col-md-5 text-center">
+            <aside id="sidebar">
+      		    <h2>Videos</h2>
+      	    </aside>
+          </div>
         </div>
-    )
+      </div>
+      )
     }
 };
 
